80f30226775e2cd3a6e8552503ea554f024c0cf1
Sidnei Pacheco <sidneip.junior@gmail.com>
2021-02-28 01:38:34 -0300

    eslint and jest and supertest config

---
+++ src/__tests__/routes/tools.routes.spec.ts
@@ -0,0 +1,8 @@
+import app from '../../app';
+import request from 'supertest'
+
+describe("First test", () => {
+  test.skip("skip", async () => {
+    expect([]).toBe([])
+  });
+});
---
+++ src/app.ts
@@ -0,0 +1,28 @@
+import 'reflect-metadata'
+import express, { NextFunction, Request, Response} from 'express'
+import 'express-async-errors';
+import routes from './routes';
+import './database'
+import AppError from './errors/AppError';
+const app = express();
+
+app.use(express.json());
+
+app.use(routes);
+
+app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
+  console.log(error)
+  if (error instanceof AppError) {
+    return response.status(error.statusCode).json({
+      status: 'error',
+      message: error.message
+    })
+  }
+
+  return response.status(500).json({
+    status: 'error',
+    message: 'internal server error'
+  })
+});
+
+export default app;
